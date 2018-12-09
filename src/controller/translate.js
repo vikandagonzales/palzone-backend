6871bd91611fd255e537b69b94c93c968a96a7ec
Wendy Parsons <wsparsons@gmail.com>
2018-12-09 10:09:28 -0800

    change translate route to get target lang

--- src/controller/translate.js
+++ src/controller/translate.js
@@ -2,9 +2,8 @@ const translateModel = require("../models/translate");
 
 async function createTranslate(req, res, next) {
   try {
-    const userId = req.params.user_id;
 
-    const response = await translateModel.createTranslate(userId, req.body);
+    const response = await translateModel.createTranslate(req.body);
 
     res.status(201).json({ response });
   } catch (err) {
