9c3104208149aadf6a051a36008296c30533871c
Mark Pavlovski <mpavlovski@zulily.com>
2018-12-08 19:51:38 -0800

    added cityguides call

--- src/models/locations.js
+++ src/models/locations.js
@@ -1,13 +1,40 @@
 const knex = require("../../db");
+const axios = require("axios");
 
 function getOneLocation(location_id) {
   // return (knex('users').where({id: user_id}).first())
 }
 
 function getAllLocations(lat, lng) {
-  // return (knex('users')
-  // .where({email: email})
-  // .first())
+  axios
+    .post(
+      "https://apis.discover.com/auth/oauth/v2/token?grant_type=client_credentials&scope=CITYGUIDES",
+      {},
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          Authorization:
+            "Basic bDd4eGFjYjE5NGY3MDUxMjQxNmE4OGZlYjdiZDAxMmE2NTg0OjM2MDliZDhmZDA5NzQyN2NhNWRlNmFkYmYyNDFmNzA0"
+        }
+      }
+    )
+    .then(async function({ data }) {
+      const { access_token } = data;
+      console.log(access_token);
+      const cities = await axios.get(
+        "https://api.discover.com/cityguides/v2/merchants?merchant_city=Los Angeles&merchant_category=restaurants",
+        {
+          headers: {
+            "x-dfs-api-plan": "CITYGUIDES_SANDBOX",
+            Authorization: `Bearer ${access_token}`
+          }
+        }
+      );
+      console.log(cities.data.result);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
   return Promise.resolve("HELLO");
 }
 
